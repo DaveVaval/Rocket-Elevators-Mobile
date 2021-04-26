237786e10fb06b1244614723b69ac777f93cc579
DaveVaval <grimmjowx9@hotmail.com>
2021-04-26 12:53:10 -0400

    Playing around

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableHighlight,
+  Image, 
+  SafeAreaView, 
+  Button
+} from 'react-native';
 
 export default function App() {
+  console.log("HELLOOO");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableHighlight onPress={() => console.log("Pressed")}>
+        <Image fadeDuration={500} source={{ width: 200, height: 300, uri: "https://picsum.photos/200/300"}} />
+      </TouchableHighlight>
+      <Button color="red" title="bruh" onPress={()=> console.log("button pressed")} />
+    </SafeAreaView>
   );
 }
 
